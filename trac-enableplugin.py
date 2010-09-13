c7534ee36ade8c0774c132eb0cb652869f83b92a
Elan Ruusamäe <glen@pld-linux.org>
2010-09-13 17:10:53 +0000

    - tracp env open exceptions (like env needs upgrade)
    Changed files:
        trac-enableplugin.py -> 1.3

--- trac-enableplugin.py
+++ trac-enableplugin.py
@@ -5,27 +5,31 @@
 # already configured.
 #
 # Author: Elan Ruusamäe <glen@delfi.ee>
-# Date: 2010-04-07
+# Date: 2010-09-13
 
 import sys
 from glob import glob
 from trac.env import open_environment
+from trac.core import TracError
 
 components = sys.argv[1:]
 for file in glob('/var/lib/trac/*/conf/trac.ini'):
-    # strip conf/trac.ini from path
-    project = file[:-14]
+	# strip conf/trac.ini from path
+	project = file[:-14]
 
-    env = open_environment(project)
+	try:
+		env = open_environment(project)
 
-    # trac/admin/web_ui.py
-    changes = False
-    for component in components:
-        is_present = env.config.has_option('components', component.lower())
-        if not is_present:
-            env.config.set('components', component, 'enabled')
-            print 'Enabling %s in %s' % (component, project)
-            changes = True
+		# trac/admin/web_ui.py
+		changes = False
+		for component in components:
+			is_present = env.config.has_option('components', component.lower())
+			if not is_present:
+				env.config.set('components', component, 'enabled')
+				print 'Enabling %s in %s' % (component, project)
+				changes = True
 
-    if changes:
-        env.config.save()
+		if changes:
+			env.config.save()
+	except TracError, e:
+		print e.message
